63093239d19e370f3bbdb0e81c2640ed6a91d569
Jorge Fernando <100095295+JorgeFernando-JF@users.noreply.github.com>
2024-10-10 17:25:30 -0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-consconst darkModeToggle = document.getElementById('darkModeToggle');
-const body = document.body;
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+        const body = document.body;
+        const texto = document.getElementById('texto-alterado');
 
-darkModeToggle.addEventListener('change', () => {
-  body.classList.toggle('dark-mode');
-});
+        darkModeToggle.addEventListener ("click", () => {
+            body.classList.toggle ("dark-mode");
+            const sunIcon = darkModeToggle.querySelector('.fas.fa-sun');
+            const moonIcon = darkModeToggle.querySelector('.fas.fa-moon');
+
+            if (body.classList.contains ("dark-mode")) {
+                sunIcon.style.display = 'none';
+                moonIcon.style.display = 'inline';
+                moonIcon.style.color = '#fff';
+                texto.textContent = 'Clique na Lua para desativar o Dark-Mode';
+            } else {
+                sunIcon.style.display = 'inline';
+                moonIcon.style.display = 'none';
+                texto.textContent = 'Clique no Sol para ativar o Dark-Mode';
+            }
+        });
